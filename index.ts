247a755a0f0672477fa60c3b0ef7f0da586a5b9f
saadbaseer <saadbaseerkhan410@gmail.com>
2024-04-14 16:53:06 +0500

    cli basses number guessing game

---
+++ index.ts
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 10) + 1;
+const answer = await inquirer.prompt([
+  {
+    name: "userGuessedNumber",
+    type: "number",
+    message: "Enter any number from 1 to 10: ",
+  },
+]);
+if (answer.userGuessedNumber === randomNumber) {
+  console.log("congratulation you guessed right number");
+} else {
+  console.log("you guessed wrong number");
+}
